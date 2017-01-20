32aa8da3184cd01a906a3f6b0c8ea3a420acb32c
Arun Donti <dontirun@gmail.com>
2017-01-20 15:09:43 -0500

    almost done with screen 4

---
+++ CS4518_Project1_Screen4/app/src/main/java/com/example/arun/cs4518_project1_screen4/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.arun.cs4518_project1_screen4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
